80617bd9d44ab8c36a8b53dd95d744b70f8dba98
Pablo Martínez <pabloextreme27@gmail.com>
2024-07-20 17:03:12 +0200

    cambios

--- js/FastUI.js
+++ js/FastUI.js
@@ -9,8 +9,12 @@ export function toggleDarkMode() {
 export function setDarkMode(dark_mode = true) {
   if (dark_mode) {
     document.documentElement.setAttribute("dark", "");
+    if (document.querySelector('meta[name="theme-color"]')) document.querySelector('meta[name="theme-color"]').setAttribute("content", "#444343");
+    if (document.querySelector('meta[name="msapplication-TileColor"]')) document.querySelector('meta[name="msapplication-TileColor"]').setAttribute("content", "#444343");
   } else {
     document.documentElement.removeAttribute("dark");
+    if (document.querySelector('meta[name="theme-color"]')) document.querySelector('meta[name="theme-color"]').setAttribute("content", "#fff");
+    if (document.querySelector('meta[name="msapplication-TileColor"]')) document.querySelector('meta[name="msapplication-TileColor"]').setAttribute("content", "#fff");
   }
 }
 
